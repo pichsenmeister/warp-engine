5971301140cdddc63ab0c3cdd788528c2ceb065e
david p <david@foo.bar>
2015-03-07 17:48:30 +0100

    new activator version

--- init.sh
+++ init.sh
@@ -6,12 +6,12 @@ apt-get -y install openjdk-7-jdk
 apt-get -y install redis-server
 apt-get -y install wget
 echo "downloading typesafe activator..."
-wget http://downloads.typesafe.com/typesafe-activator/1.2.10/typesafe-activator-1.2.10-minimal.zip
+wget http://downloads.typesafe.com/typesafe-activator/1.3.2/typesafe-activator-1.3.2-minimal.zip
 echo "unzip typesafe activator..."
-unzip typesafe-activator-1.2.10-minimal.zip
-rm typesafe-activator-1.2.10-minimal.zip
+unzip typesafe-activator-1.3.8-minimal.zip
+rm typesafe-activator-1.3.8-minimal.zip
 echo "settings PATH variables..."
-echo "export PATH=$PATH:~/activator-1.2.10-minimal/activator" >> ~/.bashrc
+echo "export PATH=$PATH:~/activator-1.3.8-minimal/activator" >> ~/.bashrc
 echo "settings aliases..."
 echo "alias warp-deploy='sh ~/warp-engine/shells/deploy.sh'" >> ~/.bashrc
 echo "alias warp-start='sh ~/warp-engine/shells/start.sh'" >> ~/.bashrc
